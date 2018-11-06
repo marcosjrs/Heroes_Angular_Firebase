4791c37ae7cf72835714cf0fc2f65d49c2c1d608
marcosjrs <marcosjrs80@gmail.com>
2018-11-06 10:31:29 +0100

    Editar heroe

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -2,29 +2,54 @@ import { Component, OnInit } from '@angular/core';
 import { Heroe } from 'src/app/models/heroe';
 import { HeroesService } from 'src/app/services/heroes.service';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
 
   heroe:Heroe;
+  idHeroe:String;
+  actualizando:boolean;
 
-  constructor(public heroesSvc:HeroesService) { 
-    this.inicializarHeroe();
-  }
+  constructor(public heroesSvc:HeroesService, public route:ActivatedRoute) { 
 
-  ngOnInit() {
+    route.params.subscribe(
+      params => {
+        if( params.id && params.id != "nuevo"){
+          this.actualizando = true;
+          this.heroesSvc.getHeroe(params.id).subscribe(
+            heroe=>{      
+              heroe["key$"] = params.id;       
+              this.inicializarHeroe(heroe);
+            }
+          );
+        }else{
+          this.inicializarHeroe();
+        }
+      }
+    );
+    
   }
   
-  inicializarHeroe(){
-    this.heroe = new Heroe("","Marvel","");
+  inicializarHeroe(heroe = null){    
+    if(heroe){
+      this.actualizando = true;
+      this.heroe = heroe;
+    }else{
+      this.heroe = new Heroe("","Marvel","");
+    }
   }
 
   guardar(){
-    this.heroesSvc.addNuevoHeroe(this.heroe).subscribe( item => console.log(item) );
+    if(this.actualizando){
+      this.heroesSvc.actualizarHeroe(this.heroe).subscribe( item => console.log(item) );
+    }else{
+      this.heroesSvc.addNuevoHeroe(this.heroe).subscribe( item => console.log(item) );
+    }
   }
   
   reset(formulario:NgForm){
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -4,13 +4,14 @@ import { Heroe } from '../models/heroe';
 
 // import { Observable } from 'rxjs/Observable';
 import { map, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
   heroesURL:string = "https://heroesapp-mjrs.firebaseio.com/heroes.json";
-  heroeURL:string = "https://heroesapp-mjrs.firebaseio.com/heroes/";
+  heroeURL:string = "https://heroesapp-mjrs.firebaseio.com/heroes";
   constructor(private httpClient:HttpClient) { 
 
   }
@@ -30,6 +31,18 @@ export class HeroesService {
   eliminarHeroe(heroe:Heroe){
     const headers = new HttpHeaders({ 'Content-Type':'application/json' });
     return this.httpClient.delete(`${this.heroeURL}/${heroe.key$}.json`,{headers});
+  }
+
+  getHeroe(id:string){
+    const headers = new HttpHeaders({ 'Content-Type':'application/json' });
+    return this.httpClient.get(`${this.heroeURL}/${id}.json`,{headers});
+  }
+
+  actualizarHeroe(heroe:Heroe){
+    // firebase no acepta que se le envie un atributo $key, porque va en la url
+    const sHeroe:string = JSON.stringify({nombre: heroe.nombre, editorial:heroe.editorial, bio:heroe.bio});
+    const headers = new HttpHeaders({ 'Content-Type':'application/json' });
+    return this.httpClient.put(`${this.heroeURL}/${heroe.key$}.json`,sHeroe,{headers});
   }
 
 
